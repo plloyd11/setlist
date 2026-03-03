4d5cfe4e72c2c8df1aecd2d42a9ea30238ed7752
plloyd11 <plloyd11@gmail.com>
2026-03-03 09:43:38 -0500

    fix auth flow

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -7,7 +7,8 @@ export const load: LayoutLoad = async ({ data, depends, fetch }) => {
 
 	const supabase = isBrowser()
 		? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_KEY, {
-				global: { fetch }
+				global: { fetch },
+				auth: { flowType: 'pkce' }
 			})
 		: createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_KEY, {
 				global: { fetch },
